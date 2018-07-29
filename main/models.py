99a4c3c9059c064aa58a2280319e7d20b75f0d01
rohitk <rohitk@xpms.io>
2018-07-29 16:40:38 +0530

    Darany project

--- main/models.py
+++ main/models.py
@@ -16,7 +16,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=120, null=False, blank=False)
     email = models.EmailField(null=False, unique=True, blank=False)
     event = models.ForeignKey(Evenet, null=True, blank=True)
-    password = models.CharField(max_length=20, null=True, blank=True)
+    password = models.CharField(max_length=50, null=True, blank=True)
     more_detail = models.TextField(null=True, blank=True)
     def __str__(self):
         return self.email
